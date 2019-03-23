2bea1525447683bd67be81d0546c173800909667
Bobby Saul <bobby.saul@yahoo.com>
2019-03-23 11:35:11 -0400

    Initial commit

---
+++ concordance.js
@@ -0,0 +1,27 @@
+var bible;
+var bibleArr = [];
+var verses = [];
+var books = [];
+(function ($) {
+    $(document).ready(function () {
+        // var bible;
+        // var bibleArr = [];
+        // var verses = [];
+        // var books = [];
+        $.ajax({
+            url : "kjv.txt",
+            success : function (data) {
+                bible = data.replace(/\r/g, "").replace(/(\w)([\n])(\w)/g, "$1 $3");
+                // $('#Bible').html(bible);
+                bibleArr = bible.split(/[\n]+/);
+                bibleArr.forEach(function(line) {
+                    if (/^[\d]+\:[\d]+/.test(line.trim())) {
+                        verses.push(line);
+                    } else {
+                        books.push(line);
+                    }
+                });
+            }
+        });
+    });
+})(jQuery)
